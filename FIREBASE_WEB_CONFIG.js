17036f1f71487ce88a828ee1fd955ccfd14373ec
KunalPatil <kunalpatil3435@gmail.com>
2026-02-28 04:42:14 +0530

    firebase configuration file

---
+++ FIREBASE_WEB_CONFIG.js
@@ -0,0 +1,15 @@
+// Firebase Web Configuration for Frontend
+// This file should be used in your React frontend (src/config/firebaseConfig.js)
+// NOT in the backend
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAt-X0Zspd4Wo3Txr3XLZ8B4UYfCIOGo48",
+  authDomain: "gofarm-41550.firebaseapp.com",
+  projectId: "gofarm-41550",
+  storageBucket: "gofarm-41550.firebasestorage.app",
+  messagingSenderId: "149293933843",
+  appId: "1:149293933843:web:7ae003a857f1762c2c26e6",
+  measurementId: "G-SV7F2WMT3B"
+};
+
+export default firebaseConfig;
